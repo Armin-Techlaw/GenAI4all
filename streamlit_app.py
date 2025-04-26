0bfc71c1dc008cb696eeb94bbd973e0226c37422
Armin <106497734+Armin-Techlaw@users.noreply.github.com>
2025-04-26 11:13:42 +0000

    settingup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+Start_here = st.Page("Allpages/Intro/Start_here.py", title="Start here", icon="🤟", default=True)
+Lesson_1 = st.Page("Allpages/Intro/Lesson_1.py", title="Lesson 1", icon="🤟", default=True)
+
+
+pg = st.navigation(
+    {
+        "Home":[Start_here],
+        "Step 1": [Lesson_1],
+    }
 )
+
+pg.run()
